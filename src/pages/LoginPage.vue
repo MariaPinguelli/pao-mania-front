3f96b256d97efb88db5ff6684a7e45c739cb7164
MariaPinguelli <mariaferpinguelli@hotmail.com>
2025-02-04 16:30:41 -0300

    template básico da página de login

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,103 @@
+<template>
+  <q-page padding class="column content-center">
+  
+    <q-card padding>
+      <q-img src="../assets/placeholder.png" />
+      <q-form ref="form" @submit="logIn">
+        <q-input 
+          label="E-mail" 
+          v-model="email"
+          type="email"
+          :rules="[
+            val => val != null || 'Campo vazio',
+            (val, rules) => rules.email(val)|| 'Digite um email válido!'
+          ]"
+          class="inBetweenInput"
+          outlined
+          dense
+        />
+        <q-input 
+          label="Senha" 
+          v-model="password"
+          :type="isPwd ? 'password' : 'text'"
+          :rules="[ val => val != null || 'Campo vazio' ]"
+          class="inBetweenInput"
+          outlined
+          dense
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+        
+        <q-btn 
+          color="grey-8" 
+          label="Entrar" 
+          type="submit"
+          :disable="isFormValid()"
+        />
+      </q-form>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+      isPwd: true
+    };
+  },
+  computed: {
+  },
+  methods: {
+    logIn(){
+      console.log("Login");
+    },
+    isFormValid(){
+      return this.$refs.form ? this.$refs.form.validate() : false;
+    }
+  },
+  created() {
+    
+  },
+};
+</script>
+
+<style scoped>
+.q-card{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 400px !important;
+  min-width: 300px !important;
+}
+
+.q-form{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.inBetweenInput{
+  /* top | right | bottom | left */
+  /* top and bottom | left and right */
+  width: 100%;
+  margin: 0.3em 1em;
+}
+
+.q-img{
+  width: 50%; 
+  margin: 3em 0;
+}
+
+.q-btn{
+  margin: 0 1em 1em 1em;
+}
+</style>

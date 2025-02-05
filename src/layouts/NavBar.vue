09e2ab885a671854abda66a17e24a50b1cd07f75
MariaPinguelli <mariaferpinguelli@hotmail.com>
2025-02-04 21:01:52 -0300

    iniciado layout

---
+++ src/layouts/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-header elevated class="bg-grey-9 text-white">
+    <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          Pão Mania
+        </q-toolbar-title>
+
+        <q-avatar
+          color="primary" 
+          text-color="white"
+          :clickable="true"
+          @click="toggleMenu"
+        >
+          <q-menu v-model="showProfileOptions">
+            <q-list style="min-width: 100px">
+              AAAAAAAAAAAAAAAAAA
+              <!-- <q-item clickable v-close-popup>
+                <q-item-section>New tab</q-item-section>
+              </q-item> -->
+            </q-list>
+          </q-menu>
+          J
+        </q-avatar>
+      </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return {
+      showProfileOptions: false
+      // leftDrawerOpen: false
+    }
+  },
+  methods: {
+    toggleLeftDrawer () {
+      this.$emit('toggleLeftDrawer')
+    },
+    toggleMenu () {
+      console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+      this.showProfileOptions = !this.showProfileOptions;
+    }
+  }
+}
+</script>
---
+++ src/layouts/SideBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-drawer 
+    show-if-above 
+    v-model="drawerOpen" 
+    side="left" 
+    elevated
+  >
+    AAAAAAAAAAAAAAAAAAAAAA
+  </q-drawer>
+</template>
+
+<script>
+
+export default {
+  props: { modelValue: { type: Boolean, default: false } },
+  // data(){
+  //   return {
+  //     // leftDrawerOpen: false
+  //   }
+  // },
+  computed: {
+    drawerOpen: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      }
+    }
+  },
+  methods: {
+    toggleLeftDrawer () {
+      this.$emit('toggleLeftDrawer')
+    }
+  }
+}
+</script>

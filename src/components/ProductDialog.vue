c742dd92e70457b92effc304c0de3c54d42f87ef
MariaPinguelli <mariaferpinguelli@hotmail.com>
2025-02-13 15:06:29 -0300

    modal de cadastro de produto, design e funções principais

--- src/components/ProductDialog.vue
+++ src/components/ProductDialog.vue
@@ -3,8 +3,8 @@
     <q-card>
       <q-card-section>
         <div class="row justify-between items-center">
-          <div class="text-h6">Editar Produto</div>
-          <div class="cursor-pointer" style="margin-left: 12em; width: fit-content;" @click="onDialogCancel"> 
+          <div class="text-h6">Adicionar Produto</div>
+          <div class="cursor-pointer" style="margin-left: 12em; width: fit-content;" @click="$emit('hide')"> 
             <q-icon name="close"/>
             Fechar
           </div>
@@ -12,73 +12,49 @@
       </q-card-section>
 
       <q-card-section>
-        <q-form ref="form" @submit="edit">
+        <q-form ref="form" @submit="registerProduct">
           <q-input 
             label="Nome" 
-            v-model="user.name"
+            v-model="newProduct.name"
             :rules="[ val => val != null || 'Campo vazio' ]"
             class="edgeInput"
             outlined
             dense
           />
+
           <q-input 
-            label="Número"
-            v-model="user.phoneNumber"
-            mask="(##) # #### - ####"
-            :rules="[ val => val.length > 0 || 'Campo vazio' ]"
-            class="inBetweenInput"
-            outlined
-            dense
-          />
-          <q-input 
-            label="E-mail" 
-            v-model="user.email"
-            type="email"
-            :rules="[
-              val => val != null || 'Campo vazio',
-              (val, rules) => rules.email(val)|| 'Digite um email válido!'
-            ]"
-            class="inBetweenInput"
-            outlined
-            dense
-          />
-          <q-input 
-            label="Senha" 
-            v-model="user.password"
-            :type="isPwd ? 'password' : 'text'"
+            label="Descrição" 
+            v-model="newProduct.description"
+            type="textarea"
             :rules="[ val => val != null || 'Campo vazio' ]"
             class="inBetweenInput"
             outlined
             dense
-          >
-            <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
-            </template>
-          </q-input>
-          <q-input 
-            label="Confirme a senha" 
-            v-model="confirmPassword"
-            :type="isPwd ? 'password' : 'text'"
-            :rules="[ 
-              val => val != null,
-              val => val === user.password
-            ]"
+          />
+
+          <q-input
+            label="Quantidade em estoque"
+            v-model.number="newProduct.quantity"
+            :rules="[ val => val != null || 'Campo vazio' ]"
+            type="number"
             class="inBetweenInput"
             outlined
             dense
-          >
-            <template v-slot:append>
-              <q-icon
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-              />
-            </template>
-          </q-input>
+          />
+
+          <q-input
+            label="Valor unitário"
+            v-model.number="newProduct.unitPrice"
+            :rules="[ val => val != null || 'Campo vazio' ]"
+            type="number"
+            class="inBetweenInput"
+            mask="#,##"
+            fill-mask="0"
+            reverse-fill-mask
+            outlined
+            dense
+          />
+
           <q-btn 
             color="grey-8" 
             label="Cadastrar" 
@@ -87,16 +63,6 @@
           />
         </q-form>
       </q-card-section>
-
-      <q-card-actions align="right">
-        <q-btn label="Excluir" color="red-8" @click="confirm('delete')" />
-
-        <q-btn 
-          :label="user.isAdmin ? 'Remover Admin' : 'Promover a Admin'"
-          :color="user.isAdmin ? 'amber-8' : 'blue-8'"
-          @click="confirm('toggleAdmin')"
-        />
-      </q-card-actions>
     </q-card>
   </q-dialog>
 </template>
@@ -108,11 +74,23 @@ export default {
   },
   data() {
     return {
-      isVisible: false
+      isVisible: false,
+      newProduct: {
+        name: null,
+        quantity: null,
+        unitPrice: null,
+        description: null
+      }
     };
   },
   methods: {
+    registerProduct(){
+      console.log("registerProduct");
+    }
     
+  },
+  created(){
+
   }
 };
 </script>

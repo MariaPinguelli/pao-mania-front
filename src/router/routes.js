71a779953ca0222092b2d345d7f545bb4f816c8e
MariaPinguelli <mariaferpinguelli@hotmail.com>
2025-02-24 22:50:55 -0300

    estruturando login e home

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,36 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/MainLayout.vue'),  // Layout principal
     children: [
-      { path: '', component: () => import('pages/RegisterPage.vue') }
+      {
+        path: '',
+        component: () => {
+          const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';  // Ou Vuex
+          return isLoggedIn
+            ? import('pages/HomePage.vue')  // Página principal
+            : import('pages/LoginPage.vue'); // Página de login
+        }
+      }
+    ]
+  },
+  
+  {
+    path: '/login',
+    component: () => import('layouts/AuthLayout.vue'), // Layout de login
+    children: [
+      {
+        path: '',
+        component: () => import('pages/LoginPage.vue')
+      }
     ]
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
+  // Sempre deixe isso por último
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
   }
-]
+];
 
-export default routes
+export default routes;

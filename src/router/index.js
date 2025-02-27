b2e7231d11bd65de297912b1abbfad3e100b5009
MariaPinguelli <mariaferpinguelli@hotmail.com>
2025-02-26 22:24:16 -0300

    vuex, axios, and store configs

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,9 @@
 import { defineRouter } from '#q-app/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
+// import { store } from 'src/store'
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
-
-export default defineRouter(function (/* { store, ssrContext } */) {
+export default defineRouter(function () {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
@@ -19,12 +11,18 @@ export default defineRouter(function (/* { store, ssrContext } */) {
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
-
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE)
   })
+
+  // Router.beforeEach((to, from, next) => {
+  //   const user = store.state.user; 
+
+  //   if (to.path === '/users' && !user?.admin) {
+  //     next('/homepage'); // Redireciona caso não seja admin
+  //   } else {
+  //     next(); // Prossegue normalmente
+  //   }
+  // });
 
   return Router
 })
